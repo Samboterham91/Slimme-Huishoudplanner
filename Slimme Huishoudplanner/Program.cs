648167dd44e3442c88b39cb3aa8f4946e6aa5bba
Samboterham91 <emchermans@gmail.com>
2025-09-22 19:08:50 +0200

    Add proper .gitignore and update Program.cs

--- Slimme Huishoudplanner/Program.cs
+++ Slimme Huishoudplanner/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Slimme_Huishoudplanner
 {
@@ -10,6 +11,34 @@ namespace Slimme_Huishoudplanner
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n--- Hoofdmenu ---");
+                Console.WriteLine("Maak een keuze: ");
+
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Ongeldige invoer. Probeer het opnieuw.");
+                    continue;
+                }
+                    switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Programma wordt afgesloten.");
+                        break;
+                    case 1:
+                        Console.WriteLine("Je hebt gekozen voor optie 1.");
+                        // Voeg hier de code toe voor optie 1
+                        break;
+                    default:
+                        Console.WriteLine("Ongeldige keuze. Probeer het opnieuw.");
+                        break;
+                }
+            } while (choice != 0);
+
         }
     }
 }
