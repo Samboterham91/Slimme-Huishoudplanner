5dad90015a70f142fac6e268f5f1e0ced31bb672
Samboterham91 <emchermans@gmail.com>
2025-10-27 12:40:27 +0100

    Nieuwe solution en project aangemaakt met juiste framework

---
+++ SlimmeHuishoudplanner/SlimmeHuishoudplanner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SlimmeHuishoudplanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
