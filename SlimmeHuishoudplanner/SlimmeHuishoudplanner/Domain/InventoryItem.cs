ddc4282d0828b5b28869cfd62d6b30f3e860a843
Samboterham91 <emchermans@gmail.com>
2025-10-27 13:58:19 +0100

    Add domain classes: User, Task, Recipe, InventoryItem, ShoppingItem

---
+++ SlimmeHuishoudplanner/SlimmeHuishoudplanner/Domain/InventoryItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimmeHuishoudplanner.Domain
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public string Unit { get; set; } = "stuks";
+
+        public void IncreaseQuantity(int amount)
+        {
+            Quantity += amount;
+        }
+
+        public void DecreaseQuantity(int amount)
+        {
+            Quantity -= amount;
+            if (Quantity < 0)
+            {
+                Quantity = 0;
+            }
+        }
+    }
+}
